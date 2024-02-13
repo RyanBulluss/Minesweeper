950cfe200b0200ba5d170fbd7c20a099ae7ce8a1
RyanBulluss <rbulluss2000@gmail.com>
2024-02-13 22:45:39 +0000

    add cell type

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { boardSize, createBoard } from './constants';
+
+type Cell = {
+  isMine: boolean;
+  isFlagged: boolean;
+  isRevealed: boolean;
+  adjacentMines: number;
+};
+
+const cell1: Cell = {
+  isMine: false,
+  isFlagged: false,
+  isRevealed: false,
+  adjacentMines: 0,
+};
+
+
 
 function App() {
+  createBoard(cell1) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-gray-700 h-[100vh]' > 
+      
     </div>
   );
 }
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+const boardSize: number = 10;
+
+function createBoard(cell: object) {
+    console.log(cell)
+}
+
+
+
+export { boardSize, createBoard };
