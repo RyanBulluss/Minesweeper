9306ea910033b71c33a1f08e5bdc62503fb188b0
RyanBulluss <rbulluss2000@gmail.com>
2024-02-15 17:01:51 +0000

    create function to calcualte adjacent bombs

--- src/GameCell.tsx
+++ src/GameCell.tsx
@@ -1,15 +1,16 @@
 interface GameCellProps {
-    content: any; // Change 'any' to the specific type of obj if possible
-  }
-  
-  const GameCell: React.FC<GameCellProps> = ({content}) => {
-  
-  
-    return (
-      <div className="bg-gray-500 border">
-        {content.isMine && 1}
-      </div>
-    )
-  }
-  
-  export default GameCell;
+  content: any; // Change 'any' to the specific type of obj if possible
+}
+
+const GameCell: React.FC<GameCellProps> = ({ content }) => {
+  return (
+    <div className="bg-gray-500 border flex justify-center items-center">
+      {!content.isMine && content.adjacentMines}
+      {content.isMine && (
+        <div className="w-3/5 h-3/5 bg-red-500 rounded-full"></div>
+      )}
+    </div>
+  );
+};
+
+export default GameCell;
--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,16 @@ import { arrayBuffer } from "stream/consumers";
 
 const boardSize: number = 10;
 const mineTotal: number = 20;
+const adjacents: any = [
+  [1, 0],
+  [0, 1],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+  [-1, 0],
+  [0, -1],
+  [-1, -1],
+];
 
 function createBoard(cell: object) {
   let newCell = JSON.parse(JSON.stringify(cell));
@@ -29,13 +39,29 @@ function addMines(arr: any) {
   const newArr = JSON.parse(JSON.stringify(arr));
   for (let i = 0; i < mineTotal; i++) {
     let valid = false;
-    let x;
-    let y;
+    let x: number;
+    let y: number;
     while (!valid) {
       y = rng(boardSize);
       x = rng(boardSize);
       if (newArr[y][x].isMine === false) {
         newArr[y][x].isMine = true;
+
+        adjacents.forEach((adj: any) => {
+          let newY = y + adj[0];
+          let newX = x + adj[1];
+            
+
+          // console.log(newArr[newY][newX]);
+          if (newY < boardSize &&
+            newY >= 0 &&
+            newX < boardSize &&
+            newX >= 0) {
+            newArr[newY][newX].adjacentMines += 1;
+            console.log(newArr[newY][newX]);
+          }
+        });
+
         valid = true;
       }
     }
@@ -43,6 +69,11 @@ function addMines(arr: any) {
   return newArr;
 }
 
-// function findAdjacent()
+function incrementAdjacent(arr: any) {
+  const newArr = JSON.parse(JSON.stringify(arr));
+  newArr.forEach((arr: any, y: number) =>
+    arr.forEach((obj: any, x: number) => {})
+  );
+}
 
 export { boardSize, createBoard, addMines, copy2DArray };
